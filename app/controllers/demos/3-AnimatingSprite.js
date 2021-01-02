f035a4bd58760ca7239d7f35a36066f768a40035
DzzD <bruno.augier@dzzd.net>
2021-01-02 01:40:15 +0100

    Third tutorial minor corrections

--- app/controllers/demos/3-AnimatingSprite.js
+++ app/controllers/demos/3-AnimatingSprite.js
@@ -1,16 +1,21 @@
+
+var sprite1Uid;
+var sprite2Uid;
+var sprite3Uid;
+var sprite4Uid;
+var width;
+
 /*
  * Init must be declared as an attribute of the Alloy tag TIGLView (eg: onInit="init")
  */
-var spriteUid;
-var width;
 function init()
 {
 	/*
 	* For Alloy project, at runtime "assets" folder become "Resources" folder
 	*/
     sprite1Uid = this.addSprite({url: "Resources/numbersBlue.png", x: 0, y: 0, width: 46, height: 46});
-    sprite2Uid = this.addSprite({url: "Resources/numbersBlue.png", x: 0, y: 50, width: 46, height: 46});
-    sprite3Uid = this.addSprite({url: "Resources/numbersBlue.png", x: 0, y: 100, width: 46, height: 46});
+    sprite2Uid = this.addSprite({url: "Resources/numbersBlue.png", x: 46, y: 0, width: 46, height: 46});
+    sprite3Uid = this.addSprite({url: "Resources/numbersBlue.png", x: 46*2, y: 0, width: 46, height: 46});
     sprite4Uid = this.addSprite({url: "Resources/spritesheet.png", x: 100, y: 50, width: 256, height: 256});
 
 
@@ -27,16 +32,7 @@ function init()
 function resize(e)
 {
     width = e.width;
-}
-
-
-/*
- * Loop must be declared as an attribute of the Alloy tag TIGLView (eg: onLoop="loop")
- */ 
-function loop()
-{
-   
-    
+    this.setEntityPositionById(sprite4Uid, width * 0.5 - 128, 50);
 }
 
 $.win.open();
