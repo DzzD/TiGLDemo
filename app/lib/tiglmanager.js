d05dccd6fa0412b79ee2eac0c3bdb4b785313f97
DzzD <bruno.augier@dzzd.net>
2021-01-04 04:36:02 +0100

    Add license

--- app/lib/tiglmanager.js
+++ app/lib/tiglmanager.js
@@ -1,3 +1,20 @@
+/*
+*	© Copyright DzzD, Bruno Augier 2013-2021 (bruno.augier@dzzd.net)
+*	 This file is part of TIGL.
+*
+*    TIGL is free software: you can redistribute it and/or modify
+*    it under the terms of the GNU General Public License as published by
+*    the Free Software Foundation, either version 3 of the License, or
+*    any later version.
+*
+*    TIGL is distributed in the hope that it will be useful,
+*    but WITHOUT ANY WARRANTY; without even the implied warranty of
+*    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*    GNU General Public License for more details.
+*
+*    You should have received a copy of the GNU General Public License
+*	 along with TIGL.  If not, see <https://www.gnu.org/licenses/>
+*/
 
 class Entity
 {
@@ -95,11 +112,11 @@ class Entity
 
 }
 
-class Sprite extends Entity
+class Sprite 
 {
     constructor(options)
     {
-        super(options)
+        super(options);
     }
 
 }
