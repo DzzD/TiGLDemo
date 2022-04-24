0b44eed77f4a77c1df9149452415a6fd2da9f168
DzzD <bruno.augier@dzzd.net>
2022-04-24 16:25:19 +0200

    Update TIGL module Update copyright

--- modules/android/fr.dzzd.tigl/1.0.0/Resources/tiglmanager.js
+++ modules/android/fr.dzzd.tigl/1.0.0/Resources/tiglmanager.js
@@ -223,6 +223,16 @@ class Sprite extends Entity
 }
 
 
+class Shape extends Entity
+{
+    constructor(options, tiglManager)
+    {
+        super(options, tiglManager);
+    }
+
+}
+
+
 class Text extends Entity
 {
     constructor(options, tiglManager)
@@ -289,7 +299,6 @@ class TIGLManager
         var self = this;
         this.tiglView.addEventListener("loopFinished", function(){self._onLoopFinished()});
         this.tiglView.addEventListener("touch", function(e){self._onTouch(e)});
-        this.timeDataSending = 0;
     }
 
     setSceneScale(sx, sy)
@@ -297,10 +306,35 @@ class TIGLManager
         this.tiglView.setSceneScale(sx, sy);
     }
 
+    addEntity(options)
+    {
+        let id = this.tiglView.addEntity(options);
+        let entity = new Entity(options, this);
+        entity.id = id;
+        let size = this.tiglView.getEntitySizeById(id);
+        entity._width = size.width;
+        entity._height = size.height;
+        this.entities.set(id, entity);
+        return entity;
+    }
+
     addSprite(options)
     {
         let id = this.tiglView.addSprite(options);
         let entity = new Sprite(options, this);
+        entity.id = id;
+        let size = this.tiglView.getEntitySizeById(id);
+        entity._width = size.width;
+        entity._height = size.height;
+        this.entities.set(id, entity);
+        return entity;
+    }
+
+    
+    addShape(options)
+    {
+        let id = this.tiglView.addShape(options);
+        let entity = new Shape(options, this);
         entity.id = id;
         let size = this.tiglView.getEntitySizeById(id);
         entity._width = size.width;
@@ -356,12 +390,12 @@ class TIGLManager
 
     _onLoopFinished()
     {
-        //Ti.API.info("onLoopFinished()");
+        // Ti.API.info("onLoopFinished()");
         
         var time = Date.now();
         this._updateSceneDatas();
         this.timeDataSend = Date.now() - time;
-        
+
     }
 
     
@@ -492,5 +526,206 @@ class TIGLManager
         }
     }
 }
+
+
+try
+{
+    const Matter = require("matter");
+    
+    
+    if(!Entity.prototype.hasOwnProperty("body"))
+    {
+        Object.defineProperty(Entity.prototype, "body", 
+        {
+            get: function() 
+            {
+                return this._body;
+            },
+    
+            set: function(value) 
+            {
+                if(this.body != null)
+                {
+                    Matter.World.remove(this.tiglManager.getWorld(), this.body);
+                    this._body = null;
+                }
+    
+                if(!value)
+                {
+                    return;
+                }
+                  
+                this._body = value;
+                Matter.World.add(this.tiglManager.getWorld(), this.body);
+            }
+        });
+    }
+       
+    
+    
+    class Physics extends TIGLManager
+    {
+    
+        constructor(tiglView, physicsTimeStep, physicsTimeStepMaxPerFrame)
+        {
+            super(tiglView);
+            this.physicsTimeStep = physicsTimeStep || 5;
+            this.physicsTimeStepMaxPerFrame = physicsTimeStepMaxPerFrame || 10;
+            this.engine = Matter.Engine.create();
+        }
+    
+        getWorld()
+        {
+            return this.engine.world;
+        }
+    
+        _insertPhysics(entity, physics)
+        {
+            if(physics)
+            {
+                 physics.angle = entity.r * Math.PI / 180.0;
+                 physics.position = {x: entity.x, y: entity.y};
+                 let type = physics.type ? physics.type : "rectangle";
+                 delete physics.type;
+                 
+                 switch(type)
+                 {
+                    case "rect" :
+                    case "rectangle" :
+                        entity.body = Matter.Bodies.rectangle(0, 0, entity.width, entity.height, physics);
+                    break;
+                    case "circle" :
+                    {
+                        let sides = physics.sides || 20;
+                        let radius = physics.radius || Math.max(entity.width * 0.5 , entity.height * 0.5);
+                        delete physics.sides;
+                        delete physics.radius;
+                        entity.body = Matter.Bodies.circle(0, 0, radius, physics, sides);
+                    }
+                    break;
+                    case "poly" :
+                    case "polygon" :
+                    {
+                        let sides = physics.sides || 3;
+                        let radius = physics.radius || Math.max(entity.width * 0.5 , entity.height * 0.5);
+                        delete physics.sides;
+                        delete physics.radius;
+                        entity.body = Matter.Bodies.polygon(0, 0, sides, radius, physics);
+                    }
+                    break;
+                    case "vertices" :
+                    {
+                        let vertices = physics.vertices;
+                        delete physics.vertices;
+                        entity.body = Matter.Bodies.fromVertices(0, 0, vertices, physics);
+                    }
+                    break;
+                 }
+                 entity.px = entity.width /2;
+                 entity.py = entity.height /2;
+                 Matter.Body.scale(entity.body, entity.sx, entity.sy)
+                 Matter.World.add(this.engine.world, entity.body);
+            }
+        }
+    
+        addEntity(options)
+        {
+           let entity = super.addEntity(options);
+           this._insertPhysics(entity, options.physics)
+           return entity;
+        }
+    
+    
+        addSprite(options)
+        {
+           let entity = super.addSprite(options);
+           this._insertPhysics(entity, options.physics)
+           return entity;
+        }
+
+        
+        addShape(options)
+        {
+           let entity = super.addShape(options);
+           this._insertPhysics(entity, options.physics)
+           return entity;
+        }
+    
+        addText(options)
+        {
+           let entity = super.addText(options);
+           this._insertPhysics(entity, options.physics)
+           return entity;
+        }
+    
+    
+        
+        removeEntityById(id)
+        {   
+            let entity = this.getEntityById(id);
+            if(entity.body)
+            {
+                Matter.World.remove(this.engine.world, entity.body);
+                entity.body = null;
+            }
+            super.removeEntityById(id);
+        }
+    
+        
+        _onLoopFinished()
+        {
+            if(!this.frameTime)
+            {
+                this.frameTime =  Date.now();
+            }
+            this.lastFrameTime = this.frameTime;
+            this.frameTime =  Date.now();
+            let frameDuration = this.frameTime - this.lastFrameTime;
+            // Ti.API.info("frameDuration:" + frameDuration + "ms");
+            var repeat= Math.floor(frameDuration/this.physicsTimeStep);
+            /*
+             * To avoid blocking app, limit the numbre of time step
+             */
+            if(repeat>this.physicsTimeStepMaxPerFrame)
+            {
+                repeat = this.physicsTimeStepMaxPerFrame;
+            }
+            var rest= frameDuration%this.physicsTimeStep;
+            this.frameTime -= rest;
+            for(var n=0;n<repeat;n++)
+            {
+                Matter.Engine.update(this.engine, this.physicsTimeStep);
+            }
+            
+            
+            for (var [id, entity] of this.entities)
+            {
+                let body = entity.body;
+                if(body)
+                {
+                    entity.x = body.position.x;
+                    entity.y = body.position.y;
+                    entity.r = body.angle * 180.0 / Math.PI;
+                }
+            }
+            super._onLoopFinished();
+        }
+    
+    }
+    
+    TIGLManager.Physics = Physics;
+    TIGLManager.Physics.Matter = Matter;
+    TIGLManager.Physics.World = Matter.World;
+    TIGLManager.Physics.Body = Matter.Body;
+    TIGLManager.Physics.Bodies = Matter.Bodies;
+    TIGLManager.Physics.Constraint = Matter.Constraint;
+    TIGLManager.Physics.Events = Matter.Events;
+    
+}
+catch(e)
+{
+    Ti.API.error("Unable to initialize 'Tigl.Physics', please make sure that 'matter.js' and 'poly-decomp.js' are available");
+}
+
 
 module.exports = TIGLManager;
--- modules/android/fr.dzzd.tigl/1.0.0/example/app.js
+++ modules/android/fr.dzzd.tigl/1.0.0/example/app.js
@@ -25,10 +25,32 @@ var tiglView =  tigl.createTIGLView(
 tiglView.addEventListener('init', function init()
 {
 	Ti.API.info("init");
-	this.addSprite({url : "Resources/dzzd.logo.png", x: 50, y:300});
-	this.setUnits("dp");
-	this.addText({font : "Resources/KidGame.fnt", text: "TEST", x: 50, y: 50, r: 10, fontSize: 64, color: "red", outlineColor: "white"});
-	this.addText({font : "Resources/KidGame.fnt", text: "WOW!", x: 50, y: 150, r: -20, fontSize: 64, color: "green", outlineColor: "white"});
+	this.addSprite({url : "Resources/appicon.png", x: 50, y: 300});
+	this.addShape({url : "Resources/appicon.png", x: 250, y: 300, tile: true, vertices: [0,0, 100,0, 100,100, 0,100] , uvs: [0,0, 100,0, 100,100, 0,100]});
+	this.addShape({url : "Resources/appicon.png", x: 50, y: 500, tile: true, vertices: [0,0, 0,100, 100,100, 100,0] , uvs: [0,0, 0,100, 100,100, 100,0]});
+
+	let vertices = new Array();
+	let uvs = new Array();
+
+	let nbVertice = 100;
+	let radius = 400;
+	for(let n = 0; n < nbVertice; n++)
+	{
+		let angleRad = (n * 360 / nbVertice) * 2.0 * Math.PI / 360;
+		let cosA = radius * Math.cos(angleRad);
+		let sinA = radius * Math.sin(angleRad);
+		vertices.push(cosA);
+		vertices.push(sinA);
+		uvs.push(cosA);
+		uvs.push(sinA);
+	}
+	let time = Date.now();
+	this.addShape({url : "Resources/box.png", x: 750, y: 500, tile: true, vertices: vertices , uvs: uvs});
+	let duration = Date.now() - time;
+	Ti.API.info(" polygone time for " + nbVertice + " vertices is " + duration + "ms" );
+	//this.units = "dp";
+	// this.addText({font : "Resources/KidGame.fnt", text: "TEST", x: 50, y: 50, r: 10, fontSize: 64, color: "red", outlineColor: "white"});
+	// this.addText({font : "Resources/KidGame.fnt", text: "WOW!", x: 50, y: 150, r: -20, fontSize: 64, color: "green", outlineColor: "white"});
 });
 
 tiglView.addEventListener('resize',function resize(e)
